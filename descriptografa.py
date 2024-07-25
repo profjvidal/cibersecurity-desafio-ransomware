a5ee7e3782c2b673d0f1440b8d28c8cc47b5cd27
Prof. JVidal <123177419+profjvidal@users.noreply.github.com>
2024-07-25 16:37:05 -0400

    Update descriptografa.py

--- descriptografa.py
+++ descriptografa.py
@@ -1,22 +1,17 @@
 import os
 import pyaes
 
-## Abre o arquivo criptografado
-file_name = "demo.txt.ransompjv"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
-
-## Chave para descriptografia
+# Chave para descriptografia
 key = b"ransomwareprofjv"
 aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
 
-## Remove o arquivo criptografado
-os.remove(file_name)
+# Abrir, ler e descriptografar o arquivo
+with open("demo.txt.ransompjv", "rb") as file:
+    decrypt_data = aes.decrypt(file.read())
 
-## Cria o arquivo descriptografado
-new_file = "demo.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+# Remover o arquivo criptografado
+os.remove("demo.txt.ransompjv")
+
+# Criar o arquivo descriptografado
+with open("demo.txt", "wb") as new_file:
+    new_file.write(decrypt_data)
